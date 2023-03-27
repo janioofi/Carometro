516b9d3c4cf7669099d24ad9ea370a30c900a732
jan1ooo <filhojanio67@gmail.com>
2023-03-26 21:57:06 -0300

    Modificando o status e acrescentando a data na tela

--- src/view/Carometro.java
+++ src/view/Carometro.java
@@ -3,6 +3,8 @@ package view;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 import java.sql.Connection;
+import java.text.DateFormat;
+import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,6 +13,10 @@ import javax.swing.border.EmptyBorder;
 import model.DAO;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
 
 public class Carometro extends JFrame {
 	
@@ -23,6 +29,8 @@ public class Carometro extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JLabel lblStatus;
+	private JLabel lblData;
 
 	/**
 	 * Launch the application.
@@ -48,6 +56,7 @@ public class Carometro extends JFrame {
 			@Override
 			public void windowActivated(WindowEvent e) {
 				status();
+				setarData();
 			}
 		});
 		setTitle("Carômetro");
@@ -59,20 +68,45 @@ public class Carometro extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.textHighlight);
+		panel.setBounds(0, 260, 624, 51);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		lblStatus = new JLabel("");
+		lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dboff.png")));
+		lblStatus.setBounds(565, 11, 32, 32);
+		panel.add(lblStatus);
+		
+		lblData = new JLabel("");
+		lblData.setForeground(SystemColor.text);
+		lblData.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblData.setBounds(25, 11, 390, 32);
+		panel.add(lblData);
 	}
 	
 	private void status() {
 		try {
 			con = dao.conectar();
 			if(con == null) {
-				System.out.println("Erro na conexão");
+				//System.out.println("Erro na conexão");
+				lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dboff.png")));
 			}else {
-				System.out.println("Conectado com sucesso");
+				//System.out.println("Conectado com sucesso");
+				lblStatus.setIcon(new ImageIcon(Carometro.class.getResource("/img/dbon.png")));
 			}
 			con.close();
 		} catch(Exception e) {
 			System.out.println(e);
 		}
 	}
-
+	
+	private void setarData() {
+		Date data = new Date();
+		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
+		lblData.setText(formatador.format(data));
+	}
 }
